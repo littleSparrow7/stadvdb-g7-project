c5643902a0090f0010be6f99e288279972d1da69
tanal <tanal@DESKTOP-G2ICFLV.local>
2022-01-29 17:31:11 +0800

    create movie class

---
+++ routes/movie.js
@@ -0,0 +1,13 @@
+class Movie {
+    constructor(id, title, year, rating, genre, first_name, last_name){
+        this.id = id;
+        this.title = title;
+        this.year = year;
+        this.rating = rating;
+        this.genre = genre;
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
+}
+
+module.exports = Movie;
