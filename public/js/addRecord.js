ac70f8e23dece4859a1763b4e6ae8f411b4fd273
tanal <tanal@DESKTOP-G2ICFLV.local>
2022-02-10 13:27:31 +0800

    remove unnecessary comments

--- public/js/addRecord.js
+++ public/js/addRecord.js
@@ -1,11 +1,14 @@
-// import { insertMovie } from "./user.js";
-
 $(document).ready(function(){
     var movie = null;
     $('#btn-add-record').click(function(){
+        //TODO: insert movie
+        /**
+         * Inserts new movie to the database
+         * @param { Movie } movie movie without id
+         */
         $.post("/node1/addMovie", movie, function(data){
             console.log(data);
+            //add to database
         });
-        alert("YAY");
     });
 });
